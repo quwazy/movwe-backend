633c6b4810a5ff376f65d62cf44592e3d6e68703
Janko Ristic <jankoristic@proton.me>
2025-05-27 19:09:30 +0200

    initial routes

--- src/main/java/movwe/controllers/MovieController.java
+++ src/main/java/movwe/controllers/MovieController.java
@@ -1,4 +1,27 @@
 package movwe.controllers;
 
+import lombok.AllArgsConstructor;
+import movwe.services.MovieService;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+@AllArgsConstructor
 public class MovieController {
+    private final MovieService movieService;
+
+    @GetMapping
+    public ResponseEntity<?> getMovies() {
+        return ResponseEntity.ok("Movies");
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createMovie() {
+        return ResponseEntity.ok("Movie created");
+    }
 }
