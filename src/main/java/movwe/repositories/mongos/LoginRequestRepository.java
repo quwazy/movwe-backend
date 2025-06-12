b4488edacbe470219b4ffb1c761c06bce380d8fa
Janko Ristic <jankoristic@proton.me>
2025-06-12 22:02:44 +0200

    additional methodes for mongo db

--- src/main/java/movwe/repositories/mongos/LoginRequestRepository.java
+++ src/main/java/movwe/repositories/mongos/LoginRequestRepository.java
@@ -4,6 +4,11 @@ import movwe.domains.mongos.LoginRequest;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface LoginRequestRepository extends MongoRepository<LoginRequest, String> {
+    Optional<List<LoginRequest>> findAllByEmail(String email);
+    Optional<List<LoginRequest>> findByEmailAndRequestTimeAfterOrderByRequestTimeAsc(String email, Long requestTime);
 }
--- src/main/java/movwe/services/mongos/LoginRequestService.java
+++ src/main/java/movwe/services/mongos/LoginRequestService.java
@@ -5,6 +5,8 @@ import movwe.domains.mongos.LoginRequest;
 import movwe.repositories.mongos.LoginRequestRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 
 @Service
 @AllArgsConstructor
@@ -13,5 +15,30 @@ public class LoginRequestService {
 
     public void save(LoginRequest loginRequest) {
         loginRequestRepository.save(loginRequest);
+    }
+
+    /**
+     * Login requests with
+     * @param email you want to check
+     * @return list of login requests
+     */
+    public List<LoginRequest> findAllByEmail(String email) {
+        return loginRequestRepository.findAllByEmail(email).orElse(null);
+    }
+
+    /**
+     * All requests with email in the last 24 hours, sorted by ascendancy
+     * @param email you want to check
+     * @return list of login request
+     */
+    public List<LoginRequest> findByEmailInLastDay(String email) {
+        return loginRequestRepository.findByEmailAndRequestTimeAfterOrderByRequestTimeAsc(email, System.currentTimeMillis()/1000L - 60 * 60 * 24).orElse(null);
+    }
+
+    /**
+     * Empty repository
+     */
+    public void deleteAll() {
+        loginRequestRepository.deleteAll();
     }
 }
