483c7f6a05b9e74b092e9055aa27275380fb5dc1
Janko Ristic <jankoristic@proton.me>
2025-06-20 13:59:04 +0200

    test client added

--- src/main/java/movwe/configs/DataConfig.java
+++ src/main/java/movwe/configs/DataConfig.java
@@ -1,6 +1,7 @@
 package movwe.configs;
 
 import lombok.AllArgsConstructor;
+import movwe.domains.clients.entities.Client;
 import movwe.domains.employees.entities.Employee;
 import movwe.domains.employees.enums.Role;
 import movwe.repositories.ClientRepository;
@@ -22,26 +23,26 @@ public class DataConfig implements CommandLineRunner {
     public void run(String... args) throws Exception {
         /// ovde popuni sifrarnike na kraju
 
-        Optional<Employee> employee = employeeRepository.findByEmail("jane06.ristic@gmail.com");
+        Optional<Employee> adminEmployee = employeeRepository.findByEmail("jane06.ristic@gmail.com");
 
-        if (employee.isEmpty()) {
-            Employee employee1 = new Employee();
-            employee1.setRole(Role.ADMIN);
-            employee1.setEmail("jane06.ristic@gmail.com");
-            employee1.setPassword(passwordEncoder.encode("12345"));
-            employee1.setFirstName("Janko");
-            employee1.setLastName("Ristic");
-            employeeRepository.saveAndFlush(employee1);
+        if (adminEmployee.isEmpty()) {
+            Employee employee = new Employee();
+            employee.setRole(Role.ADMIN);
+            employee.setEmail("jane06.ristic@gmail.com");
+            employee.setPassword(passwordEncoder.encode("12345"));
+            employee.setFirstName("Janko");
+            employee.setLastName("Ristic");
+            employeeRepository.saveAndFlush(employee);
         }
 
-//        for (int i = 0; i < 100; i++) {
-//            Employee employeeLoop = new Employee();
-//            employeeLoop.setRole(Role.EDITOR);
-//            employeeLoop.setEmail("jane" + i + "@gmail.com");
-//            employeeLoop.setPassword(passwordEncoder.encode("12345"));
-//            employeeLoop.setFirstName("Janko" + i);
-//            employeeLoop.setLastName("Ristic" + i);
-//            employeeRepository.saveAndFlush(employeeLoop);
-//        }
+        Optional<Client> testClient = clientRepository.findByEmail("test@gmail.com");
+
+        if (testClient.isEmpty()) {
+            Client client = new Client();
+            client.setEmail("test@gmail.com");
+            client.setPassword(passwordEncoder.encode("12345"));
+            client.setUsername("test");
+            clientRepository.saveAndFlush(client);
+        }
     }
 }
