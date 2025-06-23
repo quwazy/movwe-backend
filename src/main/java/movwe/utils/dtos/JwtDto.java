6d1ee22c2876e45080301672a45996fcda0f407e
Janko Ristic <jankoristic@proton.me>
2025-06-23 21:55:34 +0200

    docker compose fixes

--- src/main/java/movwe/utils/dtos/JwtDto.java
+++ src/main/java/movwe/utils/dtos/JwtDto.java
@@ -1,11 +1,13 @@
 package movwe.utils.dtos;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
+@AllArgsConstructor
 @Schema(description = "Represents JWT token for logged employee/client")
 public class JwtDto implements Serializable {
     @Schema(description = "JWT token")
