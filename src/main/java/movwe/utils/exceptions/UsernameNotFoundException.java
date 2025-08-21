27deb79469bae440cbaf0128180226f1757dcc39
quwazy <jankoristic@proton.me>
2025-08-21 19:55:24 +0200

    global exception handler class

--- src/main/java/movwe/utils/exceptions/UsernameNotFoundException.java
+++ src/main/java/movwe/utils/exceptions/UsernameNotFoundException.java
@@ -1,6 +1,7 @@
 package movwe.utils.exceptions;
 
 public class UsernameNotFoundException extends RuntimeException {
+
     public UsernameNotFoundException(String username) {
         super("User with username: " + username + " not found!");
     }
