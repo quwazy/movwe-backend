355c83a55f00624ed79d329037627f1e3a912ef6
quwazy <jankoristic@proton.me>
2025-08-21 16:28:37 +0200

    User update dto, added username

--- src/main/java/movwe/domains/users/dtos/UpdateUserDto.java
+++ src/main/java/movwe/domains/users/dtos/UpdateUserDto.java
@@ -14,6 +14,7 @@ import movwe.utils.interfaces.DtoInterface;
 public class UpdateUserDto implements DtoInterface {
     @NotNull(message = "Id is required")
     private Long id;
+    private String username;
     @Email
     private String email;
     private String password;
