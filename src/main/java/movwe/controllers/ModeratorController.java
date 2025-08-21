1669b10b95ffc6d26280e5890abd70996c4c59ab
quwazy <jankoristic@proton.me>
2025-08-21 20:03:09 +0200

    Moderator controller class modified

--- src/main/java/movwe/controllers/ModeratorController.java
+++ src/main/java/movwe/controllers/ModeratorController.java
@@ -4,12 +4,15 @@ import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import movwe.domains.moderators.dtos.CreateModeratorDto;
+import movwe.domains.moderators.dtos.ModeratorDto;
 import movwe.domains.moderators.dtos.UpdateModeratorDto;
 import movwe.services.moderatorServices.ModeratorService;
 import movwe.utils.interfaces.ControllerInterface;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -19,81 +22,53 @@ public class ModeratorController implements ControllerInterface<CreateModeratorD
     private final ModeratorService moderatorService;
 
     @Override
-    public ResponseEntity<?> getById(@PathVariable Long id) {
-        try {
-            return ResponseEntity.ok(moderatorService.getById(id));
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+    public ResponseEntity<ModeratorDto> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(moderatorService.getById(id));
     }
 
     @Override
-    public ResponseEntity<?> getAll() {
-        try {
-            return ResponseEntity.ok(moderatorService.getAll());
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+    public ResponseEntity<List<ModeratorDto>> getAll() {
+        return ResponseEntity.ok(moderatorService.getAll());
     }
 
     @Override
     public ResponseEntity<?> create(@Valid @RequestBody CreateModeratorDto dto) {
-        try {
-            if (moderatorService.create(dto) != null){
-                return ResponseEntity.ok().build();
-            }
-            return ResponseEntity.badRequest().body("Something went wrong with creating moderator");
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+        if (moderatorService.create(dto) != null){
+            return ResponseEntity.ok().build();
         }
+        return ResponseEntity.badRequest().body("Something went wrong with creating moderator");
     }
 
     @Override
     public ResponseEntity<?> update(@Valid @RequestBody UpdateModeratorDto dto) {
-        try {
-            if (moderatorService.update(dto) != null){
-                return ResponseEntity.ok().build();
-            }
-            return ResponseEntity.badRequest().body("Something went wrong with updating moderator");
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+        if (moderatorService.update(dto) != null){
+            return ResponseEntity.ok().build();
         }
+        return ResponseEntity.badRequest().body("Something went wrong with updating moderator");
     }
 
     @Operation(summary = "Changing moderator's active field")
     @PutMapping(path = "/active/{id}")
     public ResponseEntity<?> updateActivity(@PathVariable Long id){
-        try {
-            if (moderatorService.updateActivity(id) != null){
-                return ResponseEntity.ok().build();
-            }
-            return ResponseEntity.badRequest().body("Something went wrong with changing moderator's active field");
-        }catch (Exception e){
-            return ResponseEntity.badRequest().body(e.getMessage());
+        if (moderatorService.updateActivity(id) != null){
+            return ResponseEntity.ok().build();
         }
+        return ResponseEntity.badRequest().body("Something went wrong with changing moderator's active field");
     }
 
     @Override
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> deleteById(@PathVariable Long id) {
-        try {
-            if (moderatorService.deleteById(id)){
-                return ResponseEntity.ok().build();
-            }
-            return ResponseEntity.badRequest().body("Something went wrong with deleting moderator");
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
+        if (moderatorService.deleteById(id)){
+            return ResponseEntity.ok().build();
         }
+        return ResponseEntity.badRequest().body("Something went wrong with deleting moderator");
     }
 
     @Override
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<?> deleteAll() {
-        try {
-            moderatorService.deleteAll();
-            return ResponseEntity.ok().build();
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body(e.getMessage());
-        }
+    public ResponseEntity<Void> deleteAll() {
+        moderatorService.deleteAll();
+        return ResponseEntity.ok().build();
     }
 }
